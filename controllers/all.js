1241edcf1d93353b8ec20b9999403506b0bbf690
St.Onge <stonge.ms@gmail.com>
2021-07-04 11:37:43 -0700

    updated hw

--- controllers/all.js
+++ controllers/all.js
@@ -1,1 +1,31 @@
-const mysql = requ
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+
+const all = {
+  // callback as a parameter in order to redirect to main menu after selection.
+  displayAll: (callBack = () => {}) => {
+    let connection = mysql.createConnection({
+      host: "localhost",
+
+      // Your port; if not 3306
+      port: 3306,
+
+      // Your username
+      user: "root",
+
+      // Be sure to update with your own MySQL password!
+      password: "",
+      database: "employee_trackerdb",
+    });
+    let query = `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC;`;
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      callBack();
+
+    });
+    connection.end();
+  },
+};
+
+module.export = all;
---
+++ server.js
@@ -0,0 +1,3 @@
+const inquirer = require("inquirer");
+const allController = require("./controllers/all");
+
