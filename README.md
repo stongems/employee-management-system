b3526270ccc30c3a1c5bfa778ac95bc18a7ac8ae
St.Onge <stonge.ms@gmail.com>
2021-07-15 10:16:09 -0700

    Added the license badge

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# employee-management-system
+# Employee-Management-System
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const allController = require("./controllers/all");
 const mysql = require("mysql")
+const figlet = require('figlet');
+
 
 let connection = mysql.createConnection({
     host: "localhost",
@@ -13,16 +15,32 @@ let connection = mysql.createConnection({
 
     // Be sure to update with your own MySQL password!
     password: "",
-    database: "employeedb",
+    database: "employeedb"
   });
  
+// Displays start graphic and starts the init function
+const start = () => {
+    figlet('     E - M - S', {
+        font: 'Big'
+    }, (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log("\n")
+        console.log(data);
+        console.log("       ****************************************")
+        console.log("\n                    Welcome to the \n             Employee Management System!")
+        console.log("\n       ****************************************")
+        menu();
+    })
 
-
+}
+start();
 
 function menu() {
     inquirer.prompt([{
         type: "list",
-        message: "WHat do you want to do?",
+        message: "What do you want to do?",
         name: "Menu",
         choices: [
            "Add Departments",
