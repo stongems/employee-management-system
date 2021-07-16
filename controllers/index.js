740f4d2acdbf1efc7fc0dae19b1d55a18b2a3ac0
St.Onge <stonge.ms@gmail.com>
2021-07-16 09:55:25 -0700

    removed JS files stored everything into one index file

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const department = require("./department.js")
-const employee = require("./employee.js")
-const role = require("./role.js")
-const all = require("./all.js")
-
-console.log("index",role)
-
-module.exports = { all, department, employee, role };
